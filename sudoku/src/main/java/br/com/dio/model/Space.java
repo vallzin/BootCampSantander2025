91b0c8a3310161b5bb21d40432495459c8c0f98a
vallzin <valmilson@gmail.com>
2025-06-08 09:58:58 -0300

    criado package model, class Space

---
+++ sudoku/src/main/java/br/com/dio/model/Space.java
@@ -0,0 +1,37 @@
+package br.com.dio.model;
+
+public class Space {
+
+    private Integer actual;
+    private final int expected:
+    private final boolean fixed;
+
+    public Space(int expected, boolean fixed) {
+        this.expected = expected;
+        this.fixed = fixed;
+        if(fixed){
+            actual = expected;
+        }
+    }
+
+    public Integer getActual() {
+        return actual;
+    }
+
+    public void setActual(Integer actual) {
+        if(fixed) return;
+        this.actual = actual;
+    }
+
+    public int getExpected() {
+        return expected;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public void clearSpace(){
+        setActual(null);
+    }
+}
