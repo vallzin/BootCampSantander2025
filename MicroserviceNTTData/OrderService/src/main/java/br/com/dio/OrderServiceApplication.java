abad6c28582f0eb6c99e4fb94ad67548338a8347
vallzin <valmilson@gmail.com>
2025-07-03 19:44:21 -0300

    modificações OrderServiceApplication.java

--- MicroserviceNTTData/OrderService/src/main/java/br/com/dio/OrderServiceApplication.java
+++ MicroserviceNTTData/OrderService/src/main/java/br/com/dio/OrderServiceApplication.java
@@ -1,11 +1,13 @@
 package br.com.dio;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
