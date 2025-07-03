1ad2e989e4ee478500eaff3767787c15ea060a84
vallzin <valmilson@gmail.com>
2025-07-03 19:42:12 -0300

    modificações OrderController.java de OrderService

--- MicroserviceNTTData/OrderService/src/main/java/br/com/dio/controller/OrderController.java
+++ MicroserviceNTTData/OrderService/src/main/java/br/com/dio/controller/OrderController.java
@@ -1,12 +1,53 @@
 package br.com.dio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dio.model.Order;
+import br.com.dio.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pedidos")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> makeOrder(@RequestBody Order order){
+        try {
+            if (order.getCliente() == null || order.getCliente().isBlank()){
+                return ResponseEntity
+                        .badRequest()
+                        .body("Cliente é obrigatório");
+            }
+
+            if (order.getItems() == null || order.getItems().isEmpty()){
+                return ResponseEntity
+                        .badRequest()
+                        .body("Pedido deve conter itens");
+            }
+
+            Order orderProcessed = orderService.createOrder(order);
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(orderProcessed);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar pedido: " + e.getMessage());
+        }
+
+    }
+
 
     @GetMapping("/teste")
     public String test(){
